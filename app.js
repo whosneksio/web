c8c9a5d9985de9cec453846bd0b4ea08faaca0c2
whosneksio <termdcneksioxd@gmail.com>
2023-09-25 17:48:31 +0100

    Beautify code

--- app.js
+++ app.js
@@ -69,14 +69,5 @@ webSocket.addEventListener("message", (event) => {
     }
 });
 
-$(document).ready(function() {
-  $("a.scrollLink").click(function(event) {
-      event.preventDefault();
-      $("html, body").animate({
-          scrollTop: $($(this).attr("href")).offset().top
-        }, 500);
-    });
-});
-
 
 
